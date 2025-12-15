9017797eb1409e4cdd132f459079642fa4b8a491
chatapp00001-jpg <chatapp00001@gmail.com>
2025-12-16 02:00:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0.2";
+const APP_VERSION = "1.0.1"; // change this for every update
 const CACHE_NAME = "easy-chat-" + APP_VERSION;
 
 const ASSETS = [
@@ -9,34 +9,25 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", event => {
-  console.log("Installing version", APP_VERSION);
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            console.log("Deleting old cache:", key);
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
+// fetch handler
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
+
+// message listener for version
 self.addEventListener("message", event => {
   if (event.data === "GET_VERSION") {
     event.source.postMessage({ version: APP_VERSION });
